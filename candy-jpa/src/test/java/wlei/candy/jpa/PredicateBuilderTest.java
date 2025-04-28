e45c910586285922eff6ddf1faa05b25b580336e
HeLei <emailtohl@163.com>
2025-04-28 08:56:22 +0800

    关于排除软删除条件的理解

--- candy-jpa/src/test/java/wlei/candy/jpa/PredicateBuilderTest.java
+++ candy-jpa/src/test/java/wlei/candy/jpa/PredicateBuilderTest.java
@@ -13,9 +13,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static wlei.candy.jpa.SoftDeletable.PROP_DELETE_TIME;
 
 class PredicateBuilderTest {
   CriteriaBuilder cb;
@@ -41,5 +43,25 @@ class PredicateBuilderTest {
     PredicateBuilder<Long, Bid> b = new PredicateBuilder<>(cb, r);
     List<Predicate> p = b.getPredicates(qp);
     assertEquals(5, p.size());
+  }
+
+  /**
+   * 正常查询会在where条件中自动添加一条排除软删除的条件
+   * 如果手动在查询条件添加了deleteTime，那就遵循外部提供的关于deleteTime的查询，不自动添加排除软删除的条件了
+   */
+  @Test
+  void excludeSoftDeleted() {
+    QueryParameters qp = new QueryParameters().add(PROP_DELETE_TIME, LocalDateTime.now());
+    PredicateBuilder<Long, Bid> b = new PredicateBuilder<>(cb, r);
+    List<Predicate> p = b.getPredicates(qp);
+    assertFalse(p.isEmpty());
+
+    qp = new QueryParameters().setSupplement((cb, rr) -> {
+      List<Predicate> ls = new ArrayList<>();
+      ls.add(cb.isNotNull(rr.get(PROP_DELETE_TIME)));
+      return ls;
+    });
+    p = b.getPredicates(qp);
+    assertFalse(p.isEmpty());
   }
 }
